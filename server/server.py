ee8c8b8fdcc319bbf5a1ea5d3ea6036b722fb416
Haelgi <rataroleg@gmail.com>
2024-06-06 11:29:55 +0300

    add threaded

--- server/server.py
+++ server/server.py
@@ -15,11 +15,19 @@ print('Start server')
 server.listen()
 print('listen...')
 
+set_user_ip = set()  # Хранилище IP-адресов подключенных клиентов
+list_games = {}  # Хранилище игр как объектов
+gameId_userCount = 0  # Уникальный идентификатор игры / количество игроков
+
+
+def threaded_client(client_socket, player_id, game_id):
+    print('threaded_client', client_socket, player_id, game_id)
+
 
 while True:
     # Принимаем входящее соединение
     client_socket, client_address = server.accept()
-    print(f"Подключен клиент с адресом: {client_address}")
+    print(f"Connected: {client_address}")
 
     # получаем данние от клиента
     data = client_socket.recv(4096).decode()
@@ -27,6 +35,13 @@ while True:
     # если есть игрок нажал "создать игру"
     if data == 'new_game':
         print('Creating a new game')
+        gameId_userCount += 1  # Присваиваем уникальный ID новой игре
+        player_id = 0  # Указываем на первого игрока
+        game_id = gameId_userCount  # Определяем номер игры
+        # list_games[game_id] = Game(game_id)  # Создаем новую игру
+        print('Creating a new game...')
+        # Запускаем новый поток для клиента
+        threaded_client(client_socket, player_id, game_id)
 
     # если есть игрок нажал "подключится"
     if data == 'connect':
