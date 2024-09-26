832adabaad463171c6f22dba87f8d02f5714f6ff
Haelgi <rataroleg@gmail.com>
2024-09-26 17:40:12 +0300

    client side authentication is working

---
+++ script/authentication.js
@@ -0,0 +1,35 @@
+import { Player } from './player.js'
+import { players } from './player.js'
+
+
+const userName = document.getElementById('userName');
+const userPassword = document.getElementById('userPassword');
+const btn_new_game = document.getElementById('btn_new_game');
+const btn_connect = document.getElementById('btn_connect');
+
+btn_new_game.addEventListener('click', function() {
+    createNewPlayer()
+    //#TODO отправить данные на сервер на проверку
+    //#TODO если проверка провалена вывести сообщение об ошибке
+    //#TODO если проверка пройдена
+    showLobby()
+});
+
+btn_connect.addEventListener('click', function() {
+    //#TODO отправить данные на сервер на проверку
+    //#TODO если проверка провалена вывести сообщение об ошибке
+    //#TODO если проверка пройдена
+    alert(`Кооперативного режиму ще немає, але він вже в розробці!))`);
+});
+
+function createNewPlayer() {
+    //#TODO поменять название переменной игрока, и вообще лучше получить список с сервера
+    const player_0 = new Player(userName.value, userPassword.value)
+    players.push(player_0);
+    // alert(`${player_0.userName} ${player_0.userPassword}`); //проверка работы кнопки
+}
+
+function showLobby(){
+    document.querySelector('.authentication').classList.add('hidden')
+    document.querySelector('.start-game-container').classList.remove('hidden')
+}
---
+++ script/player.js
@@ -0,0 +1,9 @@
+export const players  = [];
+
+export class Player {
+
+    constructor(userName, userPassword) {
+        this.userName = userName; 
+        this.userPassword = userPassword;  
+    }
+}
