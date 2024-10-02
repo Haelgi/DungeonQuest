f368696b810c8cbfe68c23d001042c07741c392f
Haelgi <rataroleg@gmail.com>
2024-10-02 18:01:48 +0300

    scrol game to game container with created game end player obj

--- script/game_container.js
+++ script/game_container.js
@@ -1,4 +1,34 @@
-import  {game, player}  from './authentication.js';
+// import  {game, player}  from './authentication.js';
+
+// TODO удалить потом
+class Player {
+    
+    constructor() {
+        this.idx = 0; 
+        this.name = 'Олег'; 
+        this.hero = 'dwarf'; 
+        this.heroName = 'Гном-Воїн Тарвін';
+        this.authentication = true; 
+    }
+
+    checkAuthentification(){
+        return this.authentication
+    }
+};
+
+const player = new Player();
+
+class Game {
+    static gameIdx = 0;
+   
+    constructor() {
+        this.gameIdx = 0;
+        this.playerList = [player];
+        this.authentication = true; 
+    }
+}
+
+const game = new Game();
 
 export function game_container() {
     
