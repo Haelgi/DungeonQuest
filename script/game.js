a0dcea353e6cb33620eb00fd18def10252c74673
Haelgi <rataroleg@gmail.com>
2024-10-08 18:47:04 +0300

    add virtual game field

--- script/game.js
+++ script/game.js
@@ -9,8 +9,14 @@ export class Game {
 
         this.day = 0; 
         this.startFields=[[0,0], [14,0], [0,11], [14,11]]; 
-        this.openFields=[]; 
+        this.openFields=Array.from({ length: 12 }, () => Array(15).fill({}));; 
         this.room_tiles=Array.from({ length: 130 }, (_, index) => index + 1); 
     }
 }
 
+// let a = [[{},{},{}],[{},{},{}],[{},{},{}]]
+
+// a[1][0]['key'] = 'value';
+
+// console.log(a[1][0]['key'])
+// console.log(a[1][1]['key'])
