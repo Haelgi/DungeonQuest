060d7ae40bc225ef7973adf089ee470534314669
Haelgi <rataroleg@gmail.com>
2024-10-11 00:49:27 +0300

    add class and list  catacomb_cards, deadman_cards, dungeon_cards

--- script/game.js
+++ script/game.js
@@ -8,9 +8,12 @@ export class Game {
         this.authentication = false;
 
         this.day = 0; 
+        this.gameFields=Array(12).fill().map(() => Array(15).fill().map(() => ({}))); 
         this.startFields=[[0,0], [14,0], [0,11], [14,11]]; 
         this.room_tiles=Array.from({ length: 130 }, (_, index) => index + 1); 
-        this.gameFields=Array(12).fill().map(() => Array(15).fill().map(() => ({}))); 
+        this.dungeon_cards=Array.from({ length: 60 }, (_, index) => index + 1); 
+        this.catacomb_cards=Array.from({ length: 50 }, (_, index) => index + 1); 
+        this.deadman_cards=Array.from({ length: 16 }, (_, index) => index + 1); 
     }
 }
 
@@ -21,4 +24,4 @@ export class Game {
 
 // console.log(a[1][0]['key'])
 // console.log(a[1][1]['key'])
-
+// console.log(a[1][1]['key'])
