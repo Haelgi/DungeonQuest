05f2e15e49b407b2ea47f5a6be225e2c59408f35
Haelgi <rataroleg@gmail.com>
2024-10-18 15:05:26 +0300

    add keys for player

--- script/player.js
+++ script/player.js
@@ -6,9 +6,16 @@ export class Player {
         this.hero; 
         this.authentication = false;
         this.position;
+        this.keys={
+            eventOne:false,
+            eventTwo:false,
+            move:false,
+            dungeon:false,
+            search:true,
+        };
+        this.finish = false;
         this.catacomb = false;
-        this.roomIdx;
-        this.roomRotate;
         this.card_abilitie = Array.from({ length: 5 }, (_, index) => index + 1);
+        this.effectCardContainer = [];
     }
 }
