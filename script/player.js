537e257dd246dd23dff2ebfd4b782836cfc1222f
Haelgi <rataroleg@gmail.com>
2024-11-12 19:49:07 +0200

    remove this.trophyCardContainer

--- script/player.js
+++ script/player.js
@@ -15,7 +15,6 @@ class Player {
         this.effectCardContainer = [];
         this.treasureCardContainer = [];
         this.catacombCardContainer = [];
-        this.trophyCardContainer = [];
         this.eventCardContainer = [];
         this.attack = 1;
         this.extraMove = false
