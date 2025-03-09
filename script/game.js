2cac248f3301bdfb24210514b574dfb5914a90c2
Haelgi <rataroleg@gmail.com>
2025-03-09 23:18:33 +0200

    add function secretLever()

--- script/game.js
+++ script/game.js
@@ -141,7 +141,7 @@ class Game {
 
     playDungeonEvent(){
         const card = this.getRundomElement(this.dungeon_cards, dungeon_cards)   
-        ew.drawCardEW(search_cards[22]);
+        ew.drawCardEW(search_cards[23]);
         // ew.drawCardEW(card);
         // TODO включить собітия 
     }
