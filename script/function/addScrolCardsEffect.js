77256114dc63e3e43a0548a9b86ed84bfb58fde8
Haelgi <rataroleg@gmail.com>
2025-05-04 11:30:11 +0300

    added removal of a card from the deck when it is used

--- script/function/addScrolCardsEffect.js
+++ script/function/addScrolCardsEffect.js
@@ -1,6 +1,7 @@
 import { player } from '../player.js';
 import  {heroes}  from '../cards/heroes.js';
 import  {ew}  from '../eventWidows.js';
+import  {game}  from '../game.js';
 
 const handlersMap = new Map(); // Храним обработчики для каждого контейнера
 
@@ -38,10 +39,17 @@ export function addScrolCardsEffect(container, fn) {
             if (sours === 'player') activeCard = player[cardPack][cardId] 
             if (sours === 'heroes') activeCard = heroes[player.hero][cardPack][cardId] 
 
+            function useTheCard() {
+                ew.removeAllEW();
+                if (sours === 'player') player[cardPack].splice([cardId],1)
+                if (sours === 'heroes') activeCard = heroes[player.hero].splice([cardId],1)
+                game.drawTreasurePackCards()         
+                activeCard.clickFn();
+            }
             if (activeCard.clickFn) {
                 ew.drawEW('Вікорістаті цю карту?')
                 ew.drawCardsInEW(activeCard)
-                ew.addBtnInEW('btn_next','Так', activeCard.clickFn)
+                ew.addBtnInEW('btn_next','Так', useTheCard)
                 ew.addBtnInEW('btn_clouse','Ні', ew.removeAllEW)
             }
         };
